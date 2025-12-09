6893a1f48859a0e846a273c5bfb9026c06a2fce7
earnestabromjr <abromearnest@gmail.com>
2025-12-09 03:58:59 -0600

    Bump

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,16 +1,12 @@
-local wezterm = require('wezterm')
-local ssh_domains = require('extra.ssh')
-return {
-	-- color scheme
-	color_scheme = "Dark Ocean",
-	--window opacity reduced
-	window_background_opacity = .85,
-	-- make sure you use a font you have installed
-	font = wezterm.font 'Hack Nerd Font',
-	font_size = 10.5,
-	-- scroll bar
-	enable_scroll_bar = true,
-	-- Custom Key Bindings
-	-- disable_default_key_bindings = true,
-	-- ssh_domains = ssh_domains,
-}
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices.
+--
+config.font = wezterm.font("Hack Nerd Font Mono")
+config.color_scheme = "Tokyo Night Storm"
+config.window_background_opacity = 0.8
+return config
