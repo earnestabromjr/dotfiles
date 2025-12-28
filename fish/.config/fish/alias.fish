6594782b36bb5e2bc6767a3bd61ac92eb920f878
earnestabromjr <abromearnest@gmail.com>
2025-12-28 12:38:29 -0600

    added alias.fish

---
+++ fish/.config/fish/alias.fish
@@ -0,0 +1,9 @@
+alias f="fastfetch"
+alias gst="git status"
+alias ga="git add"
+alias gc="git commit -m"
+
+## Arch based aliases
+alias search="paru -Ss"
+alias install="paru -S"
+alias up="paru -Sy"
